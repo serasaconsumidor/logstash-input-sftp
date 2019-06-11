b505b6cbe055be34bb5444f6b6c787f4e41fc377
Andreza Pequeno Cruz <andreza.cruz@br.experian.com>
2019-06-10 21:05:13 -0300

    Adding ssh support

--- lib/logstash/inputs/sftp.rb
+++ lib/logstash/inputs/sftp.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 require "logstash/inputs/base"
 require "stud/interval"
-
+require "net/sftp"
+require "net/ssh"
 require "socket"
 # Generate a repeating message.
 #
@@ -44,10 +45,10 @@ class LogStash::Inputs::Sftp < LogStash::Inputs::Base
     puts "host", @host
 
     # we can abort the loop if stop? becomes true
-    @logger.info("Registering SFTP Input",
-             :username => @username, :password => @password,
-             :remote_host => @remote_host, :port => @port,
-             :remote_path => @remote_path, :local_path => @local_path)
+    # @logger.info("Registering SFTP Input",
+    #          :username => @username, :password => @password,
+    #          :remote_host => @remote_host, :port => @port,
+    #          :remote_path => @remote_path, :local_path => @local_path)
   end
 
   def run(queue)
@@ -72,7 +73,12 @@ class LogStash::Inputs::Sftp < LogStash::Inputs::Base
   def process(queue)
     puts "dentro de process"
 
-
+    Net::SSH::Transport::Algorithms::ALGORITHMS.values.each { |algs| algs.reject! { |a| a =~ /^ecd(sa|h)-sha2/ } }
+    Net::SSH::KnownHosts::SUPPORTED_TYPE.reject! { |t| t =~ /^ecd(sa|h)-sha2/ }
+    
+    Net::SFTP.start("localhost", "ftpuser", :password => "ftppass", :port => 2222)do |sftp|
+      puts "WORKED"
+    end
   end
 
   def stop
